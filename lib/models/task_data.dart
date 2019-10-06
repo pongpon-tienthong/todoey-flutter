870dfbf3a3cb9bfbbcf2baaae7a8223674087197
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2019-10-06 11:05:58 -0400

    Todoey app

---
+++ lib/models/task_data.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/foundation.dart';
+import 'package:todoey_flutter/models/task.dart';
+import 'dart:collection';
+
+class TaskData extends ChangeNotifier {
+  List<Task> _tasks = [
+    Task(name: 'AAA AAA'),
+    Task(name: 'BBB BBB'),
+    Task(name: 'CCC CCC'),
+  ];
+
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
+
+  int get taskCount {
+    return _tasks.length;
+  }
+
+  void addTask(String newTaskTitle) {
+    final Task newTask = Task(name: newTaskTitle);
+    _tasks.add(newTask);
+    notifyListeners();
+  }
+
+  void updateTask(Task task) {
+    task.toggleDone();
+    notifyListeners();
+  }
+
+  void deleteTask(Task task) {
+    _tasks.remove(task);
+    notifyListeners();
+  }
+}
